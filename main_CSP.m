85f78cf56f87dd62b18b9a530fecbfc157fdaa12
sagihaider <sagihaider@gmail.com>
2016-04-06 10:43:53 +0100

    Added files via upload
    Use main_CSP to run the f_CSP function.

---
+++ main_CSP.m
@@ -0,0 +1,10 @@
+% Call the CSP function using the left and right hand data
+    % Input
+    % C1_Data=Class 1 Data in Channels*Observations
+    % C2_Data=Class 2 Data in Channels*Observations
+
+    
+   % Output: 
+   % W=Projection Matrix Channels*Channels
+   
+[W] = f_CSP(C1_Data,C2_Data);
